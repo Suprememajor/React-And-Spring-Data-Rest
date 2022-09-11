186b117c32307c995ea46100d5184332cf11e782
Suprememajor <ndipnobertetta@gmail.com>
2022-09-11 02:28:29 +0100

    added employee domain

---
+++ src/main/java/com/supremecorp/reactandspringdatarest/domains/Employee.java
@@ -0,0 +1,35 @@
+package com.supremecorp.reactandspringdatarest.domains;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "description")
+    private String description;
+
+    public Employee(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+}
