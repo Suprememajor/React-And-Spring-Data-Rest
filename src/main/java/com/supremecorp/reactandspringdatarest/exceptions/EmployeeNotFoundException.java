98fb57ae0dede9ea062b627de5dbb559ff2513fa
Suprememajor <ndipnobertetta@gmail.com>
2022-09-11 21:35:06 +0100

    added changed EmployeeRepository to extend JpaRepository and created EmployeeNotFoundException

---
+++ src/main/java/com/supremecorp/reactandspringdatarest/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.supremecorp.reactandspringdatarest.exceptions;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(Long id) {
+
+    }
+}
